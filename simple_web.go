e98456c4385f1dfa20d16f7fc0dcfd1748a6282c
Christian Tueffers <christian.tueffers@googlemail.com>
2016-03-29 13:31:21 +0200

    Init

---
+++ simple_web.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", hello)
+    log.Fatal(http.ListenAndServe(":8079", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello World!")
+}
+
